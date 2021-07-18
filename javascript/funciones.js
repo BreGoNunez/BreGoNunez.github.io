7603d4a17514a9a06ead4271e600fd52b040adda
Brenedin <brenedingomez@protonmail.com>
2021-07-18 01:46:34 -0600

    Se suben, pero no como imagenes

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -24,7 +24,7 @@ var reader;
     }
     
 } */
-
+//-----------Proceso de seleccion de imagen--------------
 function seleccionar(event) {
     var file = event.target.files[0];
     var reader = new FileReader();
@@ -35,3 +35,34 @@ function seleccionar(event) {
   
     reader.readAsDataURL(file);
   }
+
+  //-----------Proceso de upload de imagen--------------
+//-----------upload de imagen--------------
+  document.getElementById('upload').onclick=function(){
+    ImgName= document.getElementById('namebox').value;
+    var uploadTask= firebase.storage().ref('Imagenes/'+ImgName+'.png').put(files[0]);
+    var res = encodeURI(ImgName);
+    console.log(res);
+
+    uploadTask.on('state_changed',function(snapshot){
+        var progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+        document.getElementById('upProgress').innerHTML= 'Upload'+progress+'%'; //el innerHTML y esta linea es para estar reemplazando 
+ //-----------error handling -------------                                                            //la etiqueta con cada porcentaje nuevo
+    },
+function(error){
+    alert('Error en el proceso de guardado');
+},
+ //-----------submit image link a la database------------- 
+function(){
+        uploadTask.snapshot.ref.getDownloadURL().then(function(url){  //conseguir la URL de la imagen guardada del storage
+            ImgUrl= url;
+        
+        firebase.database().ref('Pictures/'+ImgName).set({   //guardar la URL en el database de Firebase
+            Name: ImgName,
+            Link:ImgUrl
+        });
+        alert('Imagen agregada con exito');
+}
+    );
+});
+}
